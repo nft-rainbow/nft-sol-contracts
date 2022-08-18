30cad99abf9e4d787efa631f0696ddbe27977567
0x-antoni <antoni_0x@163.com>
2022-08-18 16:32:19 +0800

    remove useless

--- contracts/lib/GranularRoles.sol
+++ contracts/lib/GranularRoles.sol
@@ -5,20 +5,17 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 abstract contract GranularRoles is AccessControl {
 	// Roles list
-	// Admin role can have 2 addresses:
-	// one address same as (_owner) which can be changed
-	// one for NFTPort API access which can only be revoked
 	bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-	// Following roles can have multiple addresses, can be changed by admin or update contrac role
 	bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
 	
 	mapping(bytes32 => bool) internal _rolesFrozen;
 
 
 	function initalize() virtual internal {
+		_setupRole(DEFAULT_ADMIN_ROLE,msg.sender);
 		_setRoleAdmin(MINT_ROLE, ADMIN_ROLE);
 	}
-	
+
 	function _initRoles(address[] memory owners) internal {
 		for (uint256 i = 0; i < owners.length; i++) {
 			_setupRole(MINT_ROLE, owners[i]);
