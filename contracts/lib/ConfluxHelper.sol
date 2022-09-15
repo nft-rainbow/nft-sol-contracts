3b28aac0a87f6882c6d2b04afe9b1b58d42571d1
0x-antoni <antoni_0x@163.com>
2022-09-15 17:24:15 +0800

    support add/remove/list sponsor whitelist

--- contracts/lib/ConfluxHelper.sol
+++ contracts/lib/ConfluxHelper.sol
@@ -3,8 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@confluxfans/contracts/utils/ERC1820Context.sol";
 import "@confluxfans/contracts/InternalContracts/InternalContractsLib.sol";
+import "./GranularRoles.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-contract ConfluxHelper is ERC1820Context {
+contract ConfluxHelper is ERC1820Context, AccessControl {
+	using EnumerableSet for EnumerableSet.AddressSet;
+
+	address public constant ZERO = address(0);
+	EnumerableSet.AddressSet sponsorWhitelist;
+
 	function _setWhitelistByAdmin(address targetContract, address user) internal {
 		if (!_isCfxChain()) {
 			return;
@@ -21,11 +28,43 @@ contract ConfluxHelper is ERC1820Context {
 		}
 		address[] memory users = new address[](1);
 		users[0] = user;
-		InternalContracts.SPONSOR_CONTROL.addPrivilege(users);
-		require(InternalContracts.SPONSOR_CONTROL.isWhitelisted(address(this), user), "failed to set white list");
+		_addSponsorPrivilege(users);
 	}
 
 	function _setWhiteListForAllUser() internal {
 		_setWhiteListForUser(address(0));
+	}
+
+	function _setAdminToNone(address contractAddr) internal {
+		InternalContracts.ADMIN_CONTROL.setAdmin(contractAddr, ZERO);
+		require(InternalContracts.ADMIN_CONTROL.getAdmin(contractAddr) == ZERO, "failed to set admin to none");
+	}
+
+	function _addSponsorPrivilege(address[] memory whites) internal {
+		if (!_isCfxChain()) {
+			return;
+		}
+		InternalContracts.SPONSOR_CONTROL.addPrivilege(whites);
+		for (uint256 i = 0; i < whites.length; i++) {
+			sponsorWhitelist.add(whites[i]);
+			require(
+				InternalContracts.SPONSOR_CONTROL.isWhitelisted(address(this), whites[i]),
+				"failed to set white list"
+			);
+		}
+	}
+
+	function _removeSponsorPrivilege(address[] memory whites) internal {
+		if (!_isCfxChain()) {
+			return;
+		}
+		InternalContracts.SPONSOR_CONTROL.removePrivilege(whites);
+		for (uint256 i = 0; i < whites.length; i++) {
+			sponsorWhitelist.remove(whites[i]);
+		}
+	}
+
+	function listSponsorPrivilege() public view returns (address[] memory) {
+		return sponsorWhitelist.values();
 	}
 }
