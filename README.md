92b0a260ce955795f06b722de793ae818b586268
jiuhuadao <jiuhuadao@proton.me>
2023-01-30 18:12:05 +0800

    deploy sponsor utility

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # NFTRainbow NFT solidity contracts
 
 This project contains the NFT solidity contract templates
+
+## Utility Deployment
+
+### ContractSponsorInfo
+
+* Mainnet: `cfx:acdw2dtjbda4f805tp4mkkzrvmk8yyx2eysen3w8c0`
+* Testnet: `cfxtest:acgvuvrvk84hjzhsh0v0zuy6bk1zenrwv69ay2n75n`
--- contracts/utils/ContractSponsorInfo.sol
+++ contracts/utils/ContractSponsorInfo.sol
@@ -4,55 +4,52 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@confluxfans/contracts/InternalContracts/SponsorWhitelistControl.sol";
 
 contract ContractSponsorInfo {
-    struct SponsorInfo{
-        uint256 gasBalnce;
-        address gasSponsor;
-        uint256 gasFeeUpperBound;
-        address collateralSponsor;
-        uint256 collateralBalance;
-        bool    isAllWhiteListed;
-    }
+	struct SponsorInfo {
+		uint256 gasBalance;
+		address gasSponsor;
+		uint256 gasFeeUpperBound;
+		address collateralSponsor;
+		uint256 collateralBalance;
+		bool isAllWhiteListed;
+	}
 
-    SponsorWhitelistControl cpc = SponsorWhitelistControl(0x0888000000000000000000000000000000000001);
+	SponsorWhitelistControl cpc = SponsorWhitelistControl(0x0888000000000000000000000000000000000001);
 
-    function getSponsorInfos(
-        address[] memory addresses
-    )public view returns (SponsorInfo[] memory){
-        SponsorInfo[] memory SponsorInfos = new SponsorInfo[](uint(addresses.length));
+	function getSponsorInfos(address[] memory addresses) public view returns (SponsorInfo[] memory) {
+		SponsorInfo[] memory SponsorInfos = new SponsorInfo[](uint(addresses.length));
 
-        for (uint i = 0; i < addresses.length; i ++) {
-            require(isContract(addresses[i]));
-            SponsorInfos[i] = getSponsorInfo(addresses[i]);
-        }
+		for (uint i = 0; i < addresses.length; i++) {
+			SponsorInfos[i] = getSponsorInfo(addresses[i]);
+		}
 
-        return SponsorInfos;
-    }
+		return SponsorInfos;
+	}
 
-    function getSponsorInfo(
-        address target
-    )public view returns (SponsorInfo memory){
-        require(isContract(target), "The address should be contract address");
+	function getSponsorInfo(address target) public view returns (SponsorInfo memory) {
+		require(isContract(target), "The address should be contract address");
 
-        address gasSponsor = cpc.getSponsorForGas(target);
-        uint256 gasBalnce  = cpc.getSponsoredBalanceForGas(target);
-        uint256 gasFeeUpperBound = cpc.getSponsoredGasFeeUpperBound(target);
-        address collateralSponsor = cpc.getSponsorForCollateral(target);
-        uint256 collateralBalance = cpc.getSponsoredBalanceForCollateral(target);
-        bool    isAllWhiteListed  = cpc.isAllWhitelisted(target);
-        SponsorInfo memory res    = SponsorInfo(
-            gasBalnce,
-            gasSponsor,
-            gasFeeUpperBound,
-            collateralSponsor,
-            collateralBalance,
-            isAllWhiteListed
-            );
-        return res;
-    }
+		address gasSponsor = cpc.getSponsorForGas(target);
+		uint256 gasBalnce = cpc.getSponsoredBalanceForGas(target);
+		uint256 gasFeeUpperBound = cpc.getSponsoredGasFeeUpperBound(target);
+		address collateralSponsor = cpc.getSponsorForCollateral(target);
+		uint256 collateralBalance = cpc.getSponsoredBalanceForCollateral(target);
+		bool isAllWhiteListed = cpc.isAllWhitelisted(target);
+		SponsorInfo memory res = SponsorInfo(
+			gasBalnce,
+			gasSponsor,
+			gasFeeUpperBound,
+			collateralSponsor,
+			collateralBalance,
+			isAllWhiteListed
+		);
+		return res;
+	}
 
-    function isContract(address addr) private view returns (bool) {
-        uint size;
-        assembly { size := extcodesize(addr) }
-        return size > 0;
-   }
+	function isContract(address addr) private view returns (bool) {
+		uint size;
+		assembly {
+			size := extcodesize(addr)
+		}
+		return size > 0;
+	}
 }
